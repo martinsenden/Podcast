aa0f6239594c9891fa8bf00deac295ced995d4a4
martinsenden <martinsenden@users.noreply.github.com>
2017-12-12 17:23:37 +0100

    Podcast list

--- Podcast/PodcastListViewController.swift
+++ Podcast/PodcastListViewController.swift
@@ -8,7 +8,23 @@
 
 import UIKit
 
-class PodcastListViewController: UIViewController {
+class PodcastListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var podcastTableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = podcastTableView.dequeueReusableCell(withIdentifier: "PodcastCell")
+        
+        cell?.textLabel?.text = "Röv"
+        return cell!
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
