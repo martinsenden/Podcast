4db2cbbb58741fd8b1afcf97b8e251f9b7df5fec
AigerimBazarbayeva <aigerim.bazarbayeva@nu.edu.kz>
2017-12-15 14:37:13 +0100

    Added webkit webview to search tab

---
+++ Podcast/SearchViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  web load
+//
+//  Created by Admin on 14/12/2017.
+//  Copyright © 2017 at.fhooe.mcm. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class SearchViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://www.listennotes.com")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
