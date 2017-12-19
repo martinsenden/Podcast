41f7313e7c444446deb5c0fe273d3f9dd5c1eda9
AigerimBazarbayeva <aigerim.bazarbayeva@nu.edu.kz>
2017-12-20 00:43:01 +0100

    Playing music from remote URL

--- Podcast/PlayViewController.swift
+++ Podcast/PlayViewController.swift
@@ -7,14 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayViewController: UIViewController{
+    
+    var player: AVPlayer?
+    var playerItem: AVPlayerItem?
+    var playButton: UIButton?
     
     override func viewDidLoad(){
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let url = URL(string: "http://traffic.libsyn.com/heatrocks/Jay_Smooth_on_Run_DMCs__Raising_Hell_.mp3?dest-id=576777")
+        let playerItem:AVPlayerItem = AVPlayerItem(url: url!)
+        player = AVPlayer(playerItem: playerItem)
+        
+        let playerLayer=AVPlayerLayer(player: player!)
+        playerLayer.frame=CGRect(x:0, y:0, width:10, height:50)
+        self.view.layer.addSublayer(playerLayer)
+        
+        playButton = UIButton(type: UIButtonType.system) as UIButton
+        let xPostion:CGFloat = 130
+        let yPostion:CGFloat = 480
+        let buttonWidth:CGFloat = 150
+        let buttonHeight:CGFloat = 45
+        
+        playButton!.frame = CGRect(x: xPostion, y: yPostion, width: buttonWidth, height: buttonHeight)
+        playButton!.backgroundColor = UIColor.white
+        playButton!.setTitle("Play", for: UIControlState.normal)
+        playButton!.tintColor = UIColor.blue
+        playButton!.addTarget(self, action: #selector(PlayViewController.playButtonTapped(_:)), for: .touchUpInside)
+        self.view.addSubview(playButton!)
+    }
+    
     override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
+    }
+    
+    @objc
+    func playButtonTapped(_ sender:UIButton)
+    {
+        if player?.rate == 0
+        {
+            player!.play()
+            playButton!.setTitle("Play", for: UIControlState.normal)
+            playButton!.setImage(UIImage(named: "icons8-play-filled-50.png"), for: UIControlState.normal)
+            } else {
+            player!.pause()
+            playButton!.setTitle("Pause", for: UIControlState.normal)
+            playButton!.setImage(UIImage(named: "icons8-pause-filled-50.png"), for: UIControlState.normal)
+        }
     }
 }
