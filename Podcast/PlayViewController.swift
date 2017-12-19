082ab4cb69cd0f267add15be6c0493d779ce79b2
AigerimBazarbayeva <aigerim.bazarbayeva@nu.edu.kz>
2017-12-19 13:09:53 +0100

    Play View Controller almost finished

---
+++ Podcast/PlayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlayViewController.swift
+//  Podcast
+//
+//  Created by Admin on 19/12/2017.
+//  Copyright © 2017 at.fhooe.mcm. All rights reserved.
+//
+
+import UIKit
+
+class PlayViewController: UIViewController{
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+    }
+}
