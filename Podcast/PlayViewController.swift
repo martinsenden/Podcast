2b9c45e7018b822f060f5f92ae8eb23d6740ff5c
martinsenden <martin.senden@gmail.com>
2017-12-22 01:48:27 +0100

    Finished constraints and all else

--- Podcast/PlayViewController.swift
+++ Podcast/PlayViewController.swift
@@ -13,19 +13,27 @@ class PlayViewController: UIViewController{
     
     var player: AVPlayer?
     var playerItem: AVPlayerItem?
-    var playButton: UIButton?
     var rssFeed: String? = "null"
     var imageUrl: String = "null"
     
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBOutlet weak var timeSlider: UISlider!
+    
+    @IBAction func timeChanged(_ sender: UISlider) {
+        playbackSliderValueChanged(sender)
+    }
+    
+    @IBAction func playButton(_ sender: UIButton) {
+        playButtonTapped(sender)
+    }
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func sliderVolumeChanged(_ sender: UISlider) {
-        var currentValue = Float(sender.value)
+        let currentValue = Float(sender.value)
         player?.volume = currentValue
     }
     
-    var playbackSlider: UISlider?
-   
     override func viewDidLoad(){
         super.viewDidLoad()
         downloadImage(url: imageUrl)
@@ -37,39 +45,35 @@ class PlayViewController: UIViewController{
         playerLayer.frame=CGRect(x:0, y:0, width:10, height:50)
         self.view.layer.addSublayer(playerLayer)
         
-        playButton = UIButton(type: UIButtonType.system) as UIButton
+        /*playButton = UIButton(type: UIButtonType.system) as UIButton
         let xPosition:CGFloat = 130
         let yPosition:CGFloat = 480
         let buttonWidth:CGFloat = 150
         let buttonHeight:CGFloat = 45
         
-        playButton!.frame = CGRect(x: xPosition, y: yPosition, width: buttonWidth, height: buttonHeight)
-        playButton!.backgroundColor = UIColor.white
+        playButton!.frame = CGRect(x: xPosition, y: yPosition, width: buttonWidth, height: buttonHeight)*/
+        //playButton!.backgroundColor = UIColor.white
         playButton!.setImage(UIImage(named: "icons8-play-filled-50.png"), for: UIControlState.normal)
         playButton!.setTitle("Play", for: UIControlState.normal)
         playButton!.tintColor = UIColor.blue
-        playButton!.addTarget(self, action: #selector(PlayViewController.playButtonTapped(_:)), for: .touchUpInside)
-        self.view.addSubview(playButton!)
         
-        playbackSlider = UISlider(frame:CGRect(x: 50, y: 450, width: 300, height: 20))
-        playbackSlider!.minimumValue = 0
+        timeSlider!.minimumValue = 0
         
         let duration : CMTime = playerItem.asset.duration
         let seconds : Float64 = CMTimeGetSeconds(duration)
         
-        playbackSlider!.maximumValue = Float(seconds)
-        playbackSlider!.isContinuous = false
-        playbackSlider!.tintColor = UIColor.blue
+        timeSlider!.maximumValue = Float(seconds)
+        timeSlider!.isContinuous = false
+        timeSlider!.tintColor = UIColor.blue
         
-        playbackSlider?.addTarget(self, action: #selector(PlayViewController.playbackSliderValueChanged(_:)), for: .valueChanged)
-        playbackSlider!.addTarget(self, action: "playbackSliderValueChanged:", for: .valueChanged)
+        //timeSlider?.addTarget(self, action: #selector(PlayViewController.playbackSliderValueChanged(_:)), for: .valueChanged)
+        //timeSlider!.addTarget(self, action: "playbackSliderValueChanged:", for: .valueChanged)
         
-        self.view.addSubview(playbackSlider!)
         
         player!.addPeriodicTimeObserver(forInterval: CMTimeMakeWithSeconds(1, 1), queue: DispatchQueue.main) { (CMTime) -> Void in
             if self.player!.currentItem?.status == .readyToPlay {
                 let time : Float64 = CMTimeGetSeconds(self.player!.currentTime());
-                self.playbackSlider!.value = Float ( time );
+                self.timeSlider!.value = Float ( time );
             }
         }
     }
